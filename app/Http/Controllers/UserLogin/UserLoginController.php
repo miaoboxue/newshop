78d66cbc418dac6fabb0010493b0a631c90893e0
miaoboxue <949320533@qq.com>
2019-04-02 10:07:46 +0800

    login

--- app/Http/Controllers/UserLogin/UserLoginController.php
+++ app/Http/Controllers/UserLogin/UserLoginController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\UserLogin;
-
+use App\model\LoginModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use App\Http\Controllers\Controller;
 class UserLoginController extends Controller
 {
@@ -12,20 +13,36 @@ class UserLoginController extends Controller
         $email = $request -> input('email');
         $password = $request -> input('password');
 
-        $data=[
-            'emial'=>$email,
-            'password'=>$password
-        ];
-        print_r($data);
-       /* $url = "http://passport.miao629.com/apiuser";
-        $ch= curl_init();
-        curl_setopt($ch,CURLOPT_URL,$url);
-        curl_setopt($ch,CURLOPT_POST,1);
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($ch,CURLOPT_HEADER,0);
-        $res = curl_exec($ch);
-        return $res;*/
+        $userInfo=LoginModel::where(['email'=>$email])->first();
+        if(empty($userInfo)){
+            $response=[
+                'errno'=>40001,
+                'msg'=>'账号不存在',
+            ];
+            return $response;
+        }
+        if(password_verify($password,$userInfo->password)){
+            $token = substr(md5(time() . mt_rand(1,99999)),10,10);
+
+
+            $redis_token_api_login='api:user:login:token'.$userInfo->id;
+            Redis::set($redis_token_api_login,$token);
+            Redis::expire($redis_token_api_login,time()+86400);
+
+            $response=[
+                'erron'=>0,
+                'msg'=>'登录成功',
+                'token'=>$redis_token_api_login,
+                'id'=>$userInfo->id
+            ];
+
+        }else{
+            $response=[
+                'erron'=>40000,
+                'msg'=>'账号或密码错误',
+            ];
+        }
+        return $response;
     }
 
 }
---
+++ app/Model/LoginModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoginModel extends Model
+{
+    //
+    public $table = 'users';
+    public $timestamps = false;
+}
